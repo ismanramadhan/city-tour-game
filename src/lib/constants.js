264a7ad6cb2ca9e60340c20662af141506b3a7e3
Isman Ramadhan <ismanramadhan@gmail.com>
2026-02-23 10:53:25 +0700

    Increase default LOCATION_RADIUS_M to 500,000 meters

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -8,7 +8,7 @@ export const TARGET_COORDS = {
 // Bisa diatur via .env: NEXT_PUBLIC_TARGET_RADIUS (dalam meter)
 // Default 50 km = 50000 m (prototype friendly)
 const envRadius = typeof process !== "undefined" && process.env?.NEXT_PUBLIC_TARGET_RADIUS;
-export const LOCATION_RADIUS_M = envRadius ? Number(envRadius) : 50_000;
+export const LOCATION_RADIUS_M = envRadius ? Number(envRadius) : 500_000;
 
 // Total levels in the map
 export const TOTAL_LEVELS = 5;
